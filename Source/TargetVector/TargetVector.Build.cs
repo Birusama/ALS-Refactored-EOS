04a6c36fa3dc132edfc9860180c20685b1cd5505
colorindarkness <urielballinas@gmail.com>
2025-09-11 13:17:46 -0700

    Add DaySequence plugin and update Submodules

--- Source/TargetVector/TargetVector.Build.cs
+++ Source/TargetVector/TargetVector.Build.cs
@@ -8,7 +8,7 @@ public class TargetVector : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ALS", "ALSEditor", "ALSExtras", "ALSXT", "HeadMountedDisplay", "OnlineSubsystem", "OnlineSubsystemEOS"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ALS", "ALSEditor", "ALSExtras", "ALSXT", "HeadMountedDisplay", "OnlineSubsystem", "OnlineSubsystemEOS", "DaySequence", "DaySequenceEditor"});
 		PrivateDependencyModuleNames.AddRange(["HTTP"]);
 	}
 }
--- StartDedicatedServer.sh
+++ StartDedicatedServer.sh
@@ -7,14 +7,33 @@ UE5_SERVER_PATH="/path/to/YourProject/Binaries/Linux/YourProjectServer"
 
 NODE_VERSION="lts"
 
+GAME_PORT="7777"
+
+STATS_PORT="6666"
+
+REMOTE_CONTROL_PORT_HTTP="30010"
+
+REMOTE_CONTROL_PORT_WEBSOCKET="30020"
+
 # Arguments to pass to the UE5 Dedicated Server
-UE5_SERVER_ARGS="-log -port=7777" 
+UE5_ADDTL_SERVER_ARGS="-log"
+
+printf -v UE5_SERVER_ARGS "%s -port=%d -statsPort=%d\n" "$UE5_ADDTL_SERVER_ARGS" "$GAME_PORT" "$STATS_PORT"
+
+
+# Arguments to pass to the UE5 Dedicated Server
+# UE5_SERVER_ARGS="-log -port=7777" 
 # Example: -log -port=7777 -QueryPort=27015
+
+
+echo "Launching TargetVector Dedicated Server..."
+
+echo "🔎 Checking Dependencies"
 
 # Check if Node.js is installed
 if ! command -v node &> /dev/null
 then
-    echo "Node.js not found. Installing Node.js using NVM..."
+    echo "❌ Node.js not found. Installing Node.js using NVM..."
 
     # Install NVM
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
@@ -30,14 +49,14 @@ then
     # Set the installed LTS version as default
     nvm alias default "$NODE_VERSION"/*
 
-    echo "Node.js installation complete."
+    echo "✅ Node.js Installation Complete."
 else
-    echo "Node.js is already installed."
+    echo "✅ Node.js is already installed."
 fi
 
 # --- Launching UE5 Dedicated Server ---
 
-echo "Launching UE5 Dedicated Server..."
+echo "Launching TargetVector Dedicated Server..."
 
 if [ -f "$UE5_SERVER_PATH" ]; then
     "$UE5_SERVER_PATH" "$UE5_SERVER_ARGS"
